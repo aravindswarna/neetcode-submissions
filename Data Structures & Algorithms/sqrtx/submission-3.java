0a472adc7684132923f8d053068ada97edf036d4
aravindswarna <aravindswarana@gmail.com>
2026-07-08 05:59:59 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+        long n = x;
+        int l = 1;
+        int r = x/2;
+        while(l <= r){
+            int m = l + (r - l)/2;
+            long rt = (long)m* (long)m;
+
+            if(rt == n){
+                return m;
+            } else if(rt > n){
+                r = m - 1;
+            } else {
+                l = m+1;
+            }
+        }
+        return r;
+    }
+}
