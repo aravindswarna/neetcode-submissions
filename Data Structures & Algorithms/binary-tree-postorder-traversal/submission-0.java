44a6bb02bad2a1859c1afbfa6955d43dcf031f8a
aravindswarna <aravindswarana@gmail.com>
2026-07-25 06:20:02 +0530

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        recPre(root);
+        return list;
+    }
+
+    public void recPre(TreeNode root){
+        if(root == null) return;
+        recPre(root.left);
+        recPre(root.right);
+        list.add(root.val);
+    }
+}
