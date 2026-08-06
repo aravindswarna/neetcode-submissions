84c18ef5597f2edfcbd62053d428cef7cbd554eb
aravindswarna <aravindswarana@gmail.com>
2026-08-06 21:30:09 +0530

    Add: car-pooling - submission-4

---
+++ Data Structures & Algorithms/car-pooling/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        
+       int[] arr = new int[1001];
+
+       for(int[] trip: trips){
+        arr[trip[1]]+=trip[0];
+        arr[trip[2]]-=trip[0];
+       }
+       int sum = 0;
+       for(int i = 0; i<arr.length; i++){
+        arr[i]+=sum;
+        sum = arr[i];
+        if(sum > capacity)return false;
+       }
+       return true;
+
+
+    }
+}
